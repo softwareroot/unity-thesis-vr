b214b4c1bdbb2aa1410e567574489ab8ab2d4eec
Martin Gajdos <rootgam3s@gmail.com>
2020-02-18 11:16:46 +0100

    Adding a granade lanucher weapon

--- Assets/Scripts/Weapons/WeaponSwitching.cs
+++ Assets/Scripts/Weapons/WeaponSwitching.cs
@@ -5,7 +5,8 @@ public class WeaponSwitching : MonoBehaviour {
     ////////////////////////////////////////////////////////////////////////////////////
 
     public static int selectedWeapon = 0;
-    public static bool HAS_MACHINE_GUN = false, HAS_SNIPER_RIFLE = false;
+    public static bool HAS_MACHINE_GUN = false, HAS_SNIPER_RIFLE = false, 
+        HAS_GRANADE_LAUNCHER = false;
     [SerializeField] public GameObject recoil_gun1;
     private Scope scope_script;
     
@@ -14,7 +15,6 @@ public class WeaponSwitching : MonoBehaviour {
     void Start()
     {
         scope_script = recoil_gun1.GetComponent<Scope>();
-        
         SelectWeapon();
         
     }
@@ -42,13 +42,13 @@ public class WeaponSwitching : MonoBehaviour {
             else selectedWeapon--;
         }
 
-        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
         {
             if (selectedWeapon >= transform.childCount - 1) selectedWeapon = 0;
             else selectedWeapon++;
         }
 
-        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
         {
             if (selectedWeapon <= 0) selectedWeapon = transform.childCount - 1;
             else selectedWeapon--;
@@ -56,10 +56,11 @@ public class WeaponSwitching : MonoBehaviour {
 
         if (Input.GetKeyDown(KeyCode.Alpha1)) selectedWeapon = 0;
         if (Input.GetKeyDown(KeyCode.Alpha2)) selectedWeapon = 1;
+        if (Input.GetKeyDown(KeyCode.Alpha3)) selectedWeapon = 2;
         if (previousSelectedWeapon != selectedWeapon) SelectWeapon();
 
         // Auto rifle
-        if (selectedWeapon == 0) {
+        if (selectedWeapon == 0 || selectedWeapon == 2) {
             scope_script.OnUscoped();
             //scope_script.scope_ui.SetActive(false);
         }
