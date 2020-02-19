75453b5ee8fe26daf9592073b4bae19e2af3ee5a
Martin Gajdos <rootgam3s@gmail.com>
2020-02-19 08:01:17 +0100

    Adding granade shooting (still broken)

---
+++ Assets/GranadeScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityScript.Steps;
+
+public class GranadeScript : MonoBehaviour
+{
+
+    public float timeDelay = 2f;
+    private float startTimer;
+
+    public int damage = 10;
+    public float explosiveForce = 20f;
+    public float explosiveRadius = 15f;
+
+    void Update() {
+        startTimer += Time.deltaTime;
+
+        if (startTimer >= timeDelay) {
+            Explode();
+        }
+    }
+
+    private void Explode()
+    {
+        Collider[] coll = Physics.OverlapSphere(transform.position, explosiveRadius);
+
+        for (int i = 0; i < coll.Length; i++)
+        {
+            //coll[i].gameObject.GetComponent<Rigidbody>().AddExplosionForce(explosiveForce, transform.position, explosiveRadius);
+        }
+    }
+}
---
+++ Assets/Scripts/Weapons/ThrowGranade.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowGranade : MonoBehaviour {
+
+    public GameObject granadeModel;
+    public Transform hand;
+    public float throwForce = 10f;
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.G)) {
+            GameObject granade = Instantiate(granadeModel, hand.position, hand.rotation) as GameObject;
+            granade.GetComponent<Rigidbody>().AddForce((Quaternion.AngleAxis(-90, Vector3.up) * hand.forward), ForceMode.Impulse);
+        }
+    }
+}
