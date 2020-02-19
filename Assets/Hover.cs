aaf96203cb53ff7e4840d9364eabee65743238cd
Martin Gajdos <rootgam3s@gmail.com>
2020-02-19 12:41:47 +0100

    Chaning wave animation for ammo pickup object

--- Assets/Hover.cs
+++ Assets/Hover.cs
@@ -13,5 +13,9 @@ public class Hover : MonoBehaviour {
         startPosition = transform.position;
     }
 
-    void Update() { transform.position = startPosition + new Vector3(0.0f, Mathf.Sin(4 * Time.time) * 0.2f, 0.0f); }
+    void Update()
+    {
+        transform.position = startPosition + new Vector3(0.0f, Mathf.Sin(4 * Time.time) * 0.2f, 0.0f); 
+        transform.RotateAround(transform.position, transform.up, Time.deltaTime * 40);
+    }
 }
