85ae4a8eb4ab1b622acde5ccea5028748c93a58d
Martin Gajdos <rootgam3s@gmail.com>
2020-02-20 09:36:20 +0100

    Programming and adding Explosion animations

---
+++ Assets/FireballScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballScript : MonoBehaviour
+{
+    
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("COLLISIOn");
+
+    }
+}
--- Assets/GranadeScript.cs
+++ Assets/GranadeScript.cs
@@ -1,32 +1,52 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityScript.Steps;
 
 public class GranadeScript : MonoBehaviour
 {
 
+    private bool activateExplosionAnimation;
+    [SerializeField] public GameObject explosionObject;
     public float timeDelay = 2f;
     private float startTimer;
+    [SerializeField] private float lifetime;
 
     public int damage = 10;
     public float explosiveForce = 20f;
     public float explosiveRadius = 15f;
+    [SerializeField] public GameObject fireball;
 
-    void Start() { startTimer = 0; }
+    void Start()
+    {
+        activateExplosionAnimation = false;
+        StartCoroutine(WaitForExplode());
+    }
 
-    void Update() {
-        startTimer += Time.deltaTime;
-
-        if (startTimer >= timeDelay)
+    private void Update()
+    {
+        if (activateExplosionAnimation)
+            explosionObject.SetActive(true);
+        else
         {
-            startTimer = 0;
-            Explode();
+            explosionObject.SetActive(false);
         }
+    }
+
+    private IEnumerator WaitForExplode()
+    {
+        yield return new WaitForSeconds(2);
+        Explode();
+        yield return new WaitForSeconds(1);
+        activateExplosionAnimation = false;
+        Destroy(gameObject);
     }
 
     private void Explode()
     {
+        //Debug.Log("EXPLODE!");
+        activateExplosionAnimation = true;
+        // Check for nearby collisions with enemies -> kill those in range
+        
         
     }
+
 }
